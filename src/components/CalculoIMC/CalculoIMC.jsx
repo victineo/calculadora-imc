5b795aeedbf893ce0b7c70e2def661d46ec1b8b8
Victor <victorhenrique.alves.lopes@gmail.com>
2024-10-10 00:10:05 -0300

    Exibindo classificação do IMC

--- src/components/CalculoIMC/CalculoIMC.jsx
+++ src/components/CalculoIMC/CalculoIMC.jsx
@@ -3,20 +3,39 @@ import { InputField } from '../InputField/InputField'
 import './CalculoIMC.css'
 
 export function CalculoIMC() {
-    const [altura, setAltura] = useState('')
-    const [peso, setPeso] = useState('')
-    const [imc, setIMC] = useState(null)
+    const [altura, setAltura] = useState('');
+    const [peso, setPeso] = useState('');
+    const [imc, setIMC] = useState(null);
+    const [classificacao, setClassificacao] = useState('');
 
     const calcularIMC = () => {
         if (altura && peso) {
             const alturaMetros = parseFloat(altura) / 100;
             const pesoKg = parseFloat(peso);
-            const resultadoIMC = (pesoKg / (alturaMetros ** 2)).toFixed(2)
-            setIMC(resultadoIMC)
+            const resultadoIMC = (pesoKg / (alturaMetros ** 2)).toFixed(2);
+            setIMC(resultadoIMC);
+            setClassificacao(classificacaoIMC(resultadoIMC));
         } else {
             alert('Insira os valores de altura e peso.')
         }
     }
+
+    const classificacaoIMC = (valorIMC) => {
+        if (valorIMC >= 18.5 && valorIMC <= 24.9) {
+            return 'normal'
+        } else if (valorIMC >= 25.0 && valorIMC <= 29.9) {
+            return 'acima do peso'
+        } else if (valorIMC >= 30.0 && valorIMC <= 34.9) {
+            return 'obesidade grau I'
+        } else if (valorIMC >= 35.0 && valorIMC <= 39.9) {
+            return 'obesidade grau II'
+        } else if (valorIMC >= 40.0) {
+            return 'obesidade grau III'
+        } else {
+            return 'abaixo do peso'
+        }
+    }
+
     return (
         <div className='frame-calculadora'>
             <h1>Calculadora de IMC</h1>
@@ -24,7 +43,6 @@ export function CalculoIMC() {
                 <InputField
                     label='Insira sua altura (em cm)'
                     type='number'
-                    //placeholder='Altura (cm)'
                     required
                     value={altura}
                     onChange={(e) => setAltura(e.target.value)}
@@ -32,7 +50,6 @@ export function CalculoIMC() {
                 <InputField
                     label='Insira seu peso (em Kg)'
                     type='number'
-                    //placeholder='Peso (kg)'
                     required
                     value={peso}
                     onChange={(e) => setPeso(e.target.value)}
@@ -40,7 +57,12 @@ export function CalculoIMC() {
             </div>
             
             <button onClick={calcularIMC}>Calcular IMC</button>
-            {imc && <div>Seu IMC é de: {imc}</div>}
+            {imc && (
+                <div>
+                    <p>Seu IMC é de: <strong>{imc}</strong></p>
+                    <p>Classificação: <strong>{classificacao}</strong></p>
+                </div>
+            )}
         </div>
     )
 }
